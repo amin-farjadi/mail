9bed80b6558eaf630e5d761448b2a50f2681453a
amin-farjadi <amin.farjadi@outlook.com>
2020-12-23 23:56:23 +0000

    mailbox views emails, table format

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Email
+
+
+class AdminEmail(admin.ModelAdmin):
+    list_display = ('sender', 'subject', 'read', 'archived', 'timestamp')
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Email, AdminEmail)
--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -30,4 +30,38 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Creating a table
+  const tbl = document.createElement("table");
+  document.querySelector('#emails-view').append(tbl);
+  const table = document.querySelector("table");
+
+  function create_header(dict) {
+    let header = table.createTHead().insertRow();
+    for (let key of Object.keys(dict) ) {
+      let th = document.createElement("th");
+      let txt = document.createTextNode(key);
+      th.appendChild(txt);
+      header.appendChild(th);
+    }
+  }
+
+  function create_row(dict) {
+    let row = table.insertRow();
+    for (let key of Object.keys(dict) ) {
+      let cell = row.insertCell();
+      let txt = document.createTextNode(dict[key]);
+      cell.appendChild(txt);
+    }
+  }
+
+  // Show the contents of mailbox
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    console.log(emails);
+    create_header(emails[0]);
+    emails.forEach(email => create_row(email));
+  });
+  // document.querySelector('#emails-view').innerHTML += '<h4> Hi </h4>';
 }
